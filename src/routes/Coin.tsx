ac96ccdb5a82f735525f6e1cf4b1093a9c5f39fb
yurim-hgr <dbfla4618@naver.com>
2023-10-15 18:24:43 +0900

    이모지

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,11 @@
 import { useParams } from "react-router-dom";
 
-interface RouteParams{
-    coinId: string;
+interface RouteParams {
+  coinId: string;
 }
 function Coin() {
-    const {coinId} = useParams<RouteParams>();
-    return <div>coin : {coinId}</div>
-    
+  const { coinId } = useParams<RouteParams>();
+  return <div>coin : 😅{coinId}</div>;
 }
 
 export default Coin;
