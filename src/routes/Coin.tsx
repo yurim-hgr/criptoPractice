9f53582e03b0183172946bbe7b54c9f76394bfaf
yurim-hgr <dbfla4618@naver.com>
2023-10-16 18:15:16 +0900

    coin 상세페이지 데이터 fetch

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -9,6 +9,63 @@ interface RouteParams {
 interface RouteLocation {
     name : string;
 }
+
+interface InfoData{
+    id : string;
+    name : string;
+    symbol : string;
+    rank : number;
+    is_new : boolean;
+    is_active : boolean;
+    type : string;
+    logo : string;
+    description : string;
+    message : string;
+    open_source : boolean;
+    started_at : string;
+    development_status : string;
+    hardware_wallet : boolean;
+    proof_type : string;
+    org_structure : string;
+    hash_algorithm : string;
+    first_data_at : string;
+    last_data_at : string;
+}
+
+interface PriceData {
+    id : string;
+    name : string;
+    symbol : string;
+    rank : number;
+    circulating_supply : number;
+    total_supply : number;
+    max_supply : number;
+    beta_value : number;
+    first_data_at : string;
+    last_updated : string;
+    quotes : {
+        USD : {
+            ath_date : number;
+            ath_price : number;
+            market_cap : number;
+            market_cap_change_24h : number;
+            percent_change_1h : number;
+            percent_change_1y : number;
+            percent_change_6h : number;
+            percent_change_7d : number;
+            percent_change_12h : number;
+            percent_change_15m : number;
+            percent_change_24h : number;
+            percent_change_30d : number;
+            percent_change_30m : number;
+            percent_from_price_ath : number;
+            price : number;
+            volume_24h : number;
+            volume_24h_change_24h : number;
+        }
+    };
+}
+
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -37,6 +94,22 @@ function Coin() {
   const [loading , setLoading ] = useState(true);
   const {state} = useLocation<RouteLocation>();
   console.log(state.name);
+
+  const [info, setInfo ] = useState<InfoData>();
+  const [price , setPriceInfo ] = useState<PriceData>();
+  useEffect(
+    ()=> {
+        (async () => {
+            const info = await (await fetch("https://api.coinpaprika.com/v1/coins/btc-bitcoin")).json();
+            const priceDate =  await (await fetch("https://api.coinpaprika.com/v1/tickers/btc-bitcoin")).json();
+            setInfo(info);
+            setPriceInfo(priceDate)
+            setLoading(false);
+        })();
+    },[]
+  );
+console.log(info);
+console.log(price);
   return (
     <Container>
         <Header>
