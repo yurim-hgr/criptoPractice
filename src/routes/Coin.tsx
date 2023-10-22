5b2ad2783ec49e465004e7a4ff5bf4c0e0b823b8
yurim-hgr <dbfla4618@naver.com>
2023-10-22 19:51:52 +0900

    theme 인터페이스 설정

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import {Switch, Route, useLocation, useParams, Link, useRouteMatch } from "react-router-dom";
+import {
+  Switch,
+  Route,
+  useLocation,
+  useParams,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 import styled from "styled-components";
 import Chart from "./Chart";
 import Price from "./Price";
@@ -9,85 +16,84 @@ interface RouteParams {
 }
 
 interface RouteLocation {
-    name : string;
+  name: string;
 }
 
-interface InfoData{
-    id : string;
-    name : string;
-    symbol : string;
-    rank : number;
-    is_new : boolean;
-    is_active : boolean;
-    type : string;
-    logo : string;
-    description : string;
-    message : string;
-    open_source : boolean;
-    started_at : string;
-    development_status : string;
-    hardware_wallet : boolean;
-    proof_type : string;
-    org_structure : string;
-    hash_algorithm : string;
-    first_data_at : string;
-    last_data_at : string;
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
 }
 
 interface PriceData {
-    id : string;
-    name : string;
-    symbol : string;
-    rank : number;
-    circulating_supply : number;
-    total_supply : number;
-    max_supply : number;
-    beta_value : number;
-    first_data_at : string;
-    last_updated : string;
-    quotes : {
-        USD : {
-            ath_date : number;
-            ath_price : number;
-            market_cap : number;
-            market_cap_change_24h : number;
-            percent_change_1h : number;
-            percent_change_1y : number;
-            percent_change_6h : number;
-            percent_change_7d : number;
-            percent_change_12h : number;
-            percent_change_15m : number;
-            percent_change_24h : number;
-            percent_change_30d : number;
-            percent_change_30m : number;
-            percent_from_price_ath : number;
-            price : number;
-            volume_24h : number;
-            volume_24h_change_24h : number;
-        }
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: number;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
     };
+  };
 }
 
-
 const Container = styled.div`
-    padding: 0px 20px;
-    max-width: 480px;
-    margin: 0 auto;
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-    height: 15vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    `;
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Title = styled.h1`
-    color: ${props => props.theme.accentColor};
-    font-size: 48px;
-`
+  color: ${(props: { theme: { accentColor: any } }) => props.theme.accentColor};
+  font-size: 48px;
+`;
 const Loader = styled.span`
-  text-align  :center ;
+  text-align: center;
   display: block;
 `;
 
@@ -118,10 +124,9 @@ const Tabs = styled.div`
   grid-template-columns: repeat(2, 1fr);
   margin: 25px 0px;
   gap: 10px;
-  
 `;
 
-const Tab = styled.span<{$isactive : boolean}>`
+const Tab = styled.span<{ $isactive: boolean }>`
   text-align: center;
   text-transform: uppercase;
   font-size: 12px;
@@ -129,7 +134,10 @@ const Tab = styled.span<{$isactive : boolean}>`
   background-color: rgba(0, 0, 0, 0.5);
   padding: 7px 0px;
   border-radius: 10px;
-  color : ${(props) => props.$isactive ? props.theme.accentColor : props.theme.textColor};
+  color: ${(props: {
+    $isactive: any;
+    theme: { accentColor: any; textColor: any };
+  }) => (props.$isactive ? props.theme.accentColor : props.theme.textColor)};
   a {
     display: block;
   }
@@ -137,87 +145,88 @@ const Tab = styled.span<{$isactive : boolean}>`
 
 function Coin() {
   const { coinId } = useParams<RouteParams>();
-  const [loading , setLoading ] = useState(true);
-  const {state} = useLocation<RouteLocation>();
-//   console.log(state.name);
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteLocation>();
+  //   console.log(state.name);
 
-  const [info, setInfo ] = useState<InfoData>();
-  const [priceInfo , setPriceInfo ] = useState<PriceData>();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
 
   const priceMatch = useRouteMatch(`/${coinId}/price`);
   const chartMatch = useRouteMatch(`/${coinId}/chart`);
-//   console.log(priceMatch!== null);
-    useEffect(
-    ()=> {
-        (async () => {
-            const info = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-            const priceDate =  await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
-            setInfo(info);
-            setPriceInfo(priceDate)
-            setLoading(false);
-        })();
-    },[coinId]
-  );
+  //   console.log(priceMatch!== null);
+  useEffect(() => {
+    (async () => {
+      const info = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceDate = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(info);
+      setPriceInfo(priceDate);
+      setLoading(false);
+    })();
+  }, [coinId]);
 
   return (
     <Container>
-    <Header>
-      <Title>
-        {state?.name ? state.name : loading ? "Loading..." : info?.name}
-        
-      </Title>
-    </Header>
-    {loading ? (
-      <Loader>Loading...</Loader>
-    ) : (
-      <>
-        <Overview>
-          <OverviewItem>
-            <span>Rank:</span>
-            <span>{info?.rank}</span>
-          </OverviewItem>
-          <OverviewItem>
-            <span>Symbol:</span>
-            <span>${info?.symbol}</span>
-          </OverviewItem>
-          <OverviewItem>
-            <span>Open Source:</span>
-            <span>{info?.open_source ? "Yes" : "No"}</span>
-          </OverviewItem>
-        </Overview>
-        <Description>{info?.description}</Description>
-        <Overview>
-          <OverviewItem>
-            <span>Total Suply:</span>
-            <span>{priceInfo?.total_supply}</span>
-          </OverviewItem>
-          <OverviewItem>
-            <span>Max Supply:</span>
-            <span>{priceInfo?.max_supply}</span>
-          </OverviewItem>
-        </Overview>
+      <Header>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
 
-        <Tabs>
+          <Tabs>
             <Tab $isactive={chartMatch !== null}>
-                <Link to={`/${coinId}/chart`}>Chart</Link>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
             </Tab>
             <Tab $isactive={priceMatch !== null}>
-                <Link to={`/${coinId}/price`}>Price</Link>
+              <Link to={`/${coinId}/price`}>Price</Link>
             </Tab>
-        </Tabs>
+          </Tabs>
 
-        <Switch>
-          <Route path={`/${coinId}/price`}>
-            <Price />
-          </Route>
-          <Route path={`/${coinId}/chart`}>
-            <Chart />
-          </Route>
-        </Switch>
-      </>
-    )}
-  </Container>
-);
+          <Switch>
+            <Route path={`/${coinId}/price`}>
+              <Price />
+            </Route>
+            <Route path={`/${coinId}/chart`}>
+              <Chart />
+            </Route>
+          </Switch>
+        </>
+      )}
+    </Container>
+  );
 }
 
 export default Coin;
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,12 +1,11 @@
 // import original module declarations
-import 'styled-components';
-
+import "styled-components";
 
 // and extend them!
-declare module 'styled-components' {
+declare module "styled-components" {
   export interface DefaultTheme {
     textColor: string;
     bgColor: string;
-    };
-  
+    accentColor: string;
+  }
 }
