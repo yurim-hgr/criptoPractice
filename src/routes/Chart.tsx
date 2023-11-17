2869516e183758c4718131cc3414b4b1cb08bb0b
yurim-hgr <dbfla4618@naver.com>
2023-11-17 20:00:32 +0900

    feat: chart 세팅

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -5,10 +5,19 @@ import { fetchChartData } from "../api";
 interface chartInterface {
   coinId: string;
 }
-
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 function Chart({ coinId }: chartInterface) {
   const { isLoading, data } = useQuery("ohicv", () => fetchChartData(coinId));
-
+  console.log(data);
   return <h1>Chart</h1>;
 }
 
